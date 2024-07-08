5543cde9c3bc69e95601a7635e67dd5e72f76b11
georgeadono <georgeadono98@gmail.com>
2024-07-08 11:16:53 +0300

    Something-Progress

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,11 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    pc: `This is my PC. I work mostly in JavaScript these days.
       I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
       Here is my <a href="https://github.com/georgeadono" target="_blank">Github</a>!`,
     "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
     "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-    I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
     You'll learn how I built this portfolio you're currently playing through!`,
     tv: `That's my TV. I've been watching tech youtubers a lot recently like :
      <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
@@ -15,9 +14,9 @@ export const dialogueData = {
     resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
     Contact me at georgeadono98@gmail.com if you have any interesting job opportunities!`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-    Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.
+    I am also building a quiz app in React-Native at the time !`,
     library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
     I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-    exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+    exit: `If you want to exit georgeadono's portfolio, just close the tab.`,
   };
